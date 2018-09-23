2c363cf3ad085cffa8264116e8bda5b941a7ff1a
Tien Pham <r3dsky02@yahoo.com.vn>
2018-09-23 19:46:39 +0300

    render props

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import Toggle from "./Toggle";
+import Toggle from "./ToggleRenderProps";
 
 class App extends Component {
   render() {
@@ -11,9 +11,14 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <Toggle>
-          <h1>Toggle</h1>
-        </Toggle>
+        <Toggle
+          render={({ on, toggle }) => (
+            <div>
+              {on && <h1>Show me</h1>}
+              <button onClick={toggle}>Show/Hide</button>
+            </div>
+          )}
+        />
       </div>
     );
   }
