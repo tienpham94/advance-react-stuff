c40e06817a067895833eba9f5c2456f78d47a758
Tien Pham <r3dsky02@yahoo.com.vn>
2018-09-23 19:27:34 +0300

    Use children props

--- src/Toggle.js
+++ src/Toggle.js
@@ -2,13 +2,15 @@ import React, { Component } from "react";
 
 export default class Toggle extends Component {
   state = { on: false };
+
   toggle = () => {
     this.setState({ on: !this.state.on });
   };
+
   render() {
     return (
       <div>
-        {this.state.on && <h1>Toggle Me</h1>}
+        {this.state.on && this.props.children}
         <button onClick={this.toggle}>Show/Hide</button>
       </div>
     );
