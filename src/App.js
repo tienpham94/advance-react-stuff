43e06c566c6ffb9bf873e6072f98e8baf617dde2
Tien Pham <r3dsky02@yahoo.com.vn>
2018-10-06 18:24:45 +0300

    Context

--- src/App.js
+++ src/App.js
@@ -1,28 +1,52 @@
-import React, { Component, Fragment } from "react";
+import React, { Component, Fragment, createContext } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { Toggle } from "Utilities";
 import { Modal } from "Elements";
 
+const UserContext = createContext();
+
+class UserProvider extends Component {
+  state = {
+    id: "123",
+    name: "scott",
+    mail: "email"
+  };
+  render() {
+    return (
+      <UserContext.Provider value={{ user: this.state }}>
+        {this.props.children}
+      </UserContext.Provider>
+    );
+  }
+}
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <Toggle>
-          {({ on, toggle }) => (
-            <Fragment>
-              <button onClick={toggle}>Login</button>
-              <Modal on={on} toggle={toggle}>
-                <h1>Still in modal</h1>
-              </Modal>
-            </Fragment>
+      <UserProvider>
+        <UserContext.Consumer>
+          {context => (
+            <div className="App">
+              <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <h1 className="App-title">Welcome to React</h1>
+              </header>
+              <Toggle>
+                {({ on, toggle }) => (
+                  <Fragment>
+                    <button onClick={toggle}>Login</button>
+                    <Modal on={on} toggle={toggle}>
+                      <h1>Still in modal</h1>
+                    </Modal>
+                  </Fragment>
+                )}
+              </Toggle>
+              <h3>{context && context.user.name}</h3>
+            </div>
           )}
-        </Toggle>
-      </div>
+        </UserContext.Consumer>
+      </UserProvider>
     );
   }
 }
