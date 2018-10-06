c26a83811e2e19ca5a51dd4bdb45a6be2d5130ef
Tien Pham <r3dsky02@yahoo.com.vn>
2018-10-06 22:40:04 +0300

    Native dragging

--- src/Drag.js
+++ src/Drag.js
@@ -3,25 +3,28 @@ import { Gesture } from 'react-with-gesture';
 import { Spring, animated, interpolate } from 'react-spring';
 import { Card } from 'Elements';
 
+const AnimCard = Card.withComponent(animated.div);
+
 export default class Drag extends Component {
   render() {
     return (
       <Gesture>
         {({ down, xDelta }) => (
           <Spring
+            native
             to={{
               x: down ? xDelta : 0
             }}
             immediate={name => down && name === 'x'}
           >
             {({ x }) => (
-              <Card
+              <AnimCard
                 style={{
-                  transform: `translateX(${x}px)`
+                  transform: x.interpolate(x => `translateX(${x}px)`)
                 }}
               >
                 <h1>Drag Me</h1>
-              </Card>
+              </AnimCard>
             )}
           </Spring>
         )}
